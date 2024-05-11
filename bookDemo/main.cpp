c6854eb50d74a7eeae046d738559cfcdc5cce10d
luods <1008611@qq.com>
2024-05-11 11:15:52 +0800

    1

---
+++ bookDemo/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <QApplication>
+
+//#include "page01/ch101_designCal/designcaldialog.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QWidget*m_pWgt=NULL;
+    //m_pWgt=new designCalDialog;//1
+
+//    m_pWgt->show();
+
+    return a.exec();
+}
+
